05fa8687cca18398313959596d4f96ee9878b998
Ryder0208 <vitthalg123@gmail.com>
2022-07-22 09:03:19 +0530

    File Added

---
+++ src/main/java/com/vir/model/Main.java
@@ -0,0 +1,9 @@
+package com.vir.model;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****...Hello world....!****");
+        System.out.println("***..Welcome to Jenkins Tutotial...***");
+
+    }
+}
